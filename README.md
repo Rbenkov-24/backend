184295870898628160dfd02c4c3358d69cc8dabb
Ramy <176412840+Rbenkov-24@users.noreply.github.com>
2024-10-28 04:02:53 -0400

    Initial cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+## SkillStream
+
+## Overview
+
+
+
+https://www.youtube.com/watch?v=O3BUHwfHf84
+https://www.youtube.com/watch?v=DuudSp4sHmg
+https://www.youtube.com/watch?v=LLAZUTbc97I
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/dbConn.js";
-import addContentRouter from "./routes/AddContent.js";
 
-//load environment variable from .env file
+
+// Load environment variable from .env file
 dotenv.config();
 
-//create an express application
+// Create an Express application
 const app = express();
 
-//define PORT variable from envirnemnt variables, default to 4000
+// Define PORT variable from environment variables, default to 4000
 const PORT = process.env.PORT || 4000;
 
-//midlleware to parse JSON bodies
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-//use my routes
-app.use(addContentRouter);
+// Enable CORS
+app.use(cors()); // Allow CORS for all routes
 
-//route for the root URL
-// app.get("/", (req, res) => {
-//   res.send(`Hello Ramy, your server is ready!`);
-// });
 
-//connect to database and start the server
+// Use my routes prefixed with /api
+app.use();
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Connect to database and start the server
 connectDB();
 app.listen(PORT, () => {
-  console.log(`server is running on port:${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
